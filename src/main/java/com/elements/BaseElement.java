8f1e3c0dd60429fba3f56eb1624f32dcf7047f44
viktoriia_bilobram <bilobram.v@ukr.net>
2021-03-28 16:32:01 +0300

    adding isDisplayedMethod

--- src/main/java/com/elements/BaseElement.java
+++ src/main/java/com/elements/BaseElement.java
@@ -9,9 +9,14 @@ public class BaseElement {
     protected WebDriver driver;
     public WebElement webElement;
     protected By path;
-    public BaseElement(WebDriver driver, BaseLocator locator){
+
+    public BaseElement(WebDriver driver, BaseLocator locator) {
         this.driver = driver;
         this.path = locator.getPath();
         webElement = driver.findElement(path);
+    }
+
+    public boolean isDisplayed() {
+        return this.webElement.isDisplayed();
     }
 }
