c6814444789e9646f3c28415e81f42bc069d97d7
TimeKenosh <79340430+TimeKenosh@users.noreply.github.com>
2021-04-16 17:22:23 +0300

    Upgrade BasicTest + OpenPreviewPageTest

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,20 +1,13 @@
-import com.pageObject.CreateNewsPO;
-import com.pageObject.EcoNewsPO;
 import com.pageObject.LogInPO;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.*;
-import org.testng.asserts.SoftAssert;
-
-import javax.swing.*;
 
 public class BasicTest {
 
     final String email = "amelyanovich11@gmail.com";
     final String password = "Qwerty123_";
-    private static WebDriver webDriver;
+    protected static WebDriver webDriver;
 
     @BeforeClass
     public void setUpClass() {
@@ -41,26 +34,4 @@ public class BasicTest {
     public void tearDownClass(){
         webDriver.quit();
     }
-
-    //    @Test
-//    public void loginToGreenCity(){
-//        webDriver.findElement(By.xpath("//a[@role='sign in']")).click();
-//        webDriver.findElement(By.id("email")).sendKeys("amelyanovich11@gmail.com");
-//        webDriver.findElement(By.id("password")).sendKeys("Qwerty123_");
-//        webDriver.findElement(By.xpath("//button[contains(@class, 'primary-global-button') and @type='submit']")).click();
-//    }
-//    @Test
-//    public void createNews(){
-//        webDriver.findElement(By.xpath("//li[@role='eco-news']")).click();
-//        Assert.assertEquals(webDriver.findElement(By.className("main-header")).getText(),"Eco news");
-//        webDriver.findElement(By.id("create-button")).click();
-//  }
-//    @Test
-//    public void goToCreateNews() {
-//        webDriver.findElement(By.xpath("//a[@role='sign in']")).click();
-//        webDriver.findElement(By.id("email")).sendKeys("amelyanovich11@gmail.com");
-//        webDriver.findElement(By.id("password")).sendKeys("Qwerty123_");
-//        webDriver.findElement(By.xpath("//button[contains(@class, 'primary-global-button') and @type='submit']")).click();
-//        CreateNewsPO createNewsPO = new EcoNewsPO(webDriver).clickEcoNews().clickCreateNewsBtn();
-//    }
 }
--- src/test/java/OpenPreviewPageTest.java
+++ src/test/java/OpenPreviewPageTest.java
@@ -1,38 +1,9 @@
-import com.pageObject.CreateNewsPO;
 import com.pageObject.EcoNewsPO;
-import com.pageObject.LogInPO;
 import com.pageObject.PreviewPO;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class OpenPreviewPageTest {
-    final String email = "amelyanovich11@gmail.com";
-    final String password = "Qwerty123_";
-    private static WebDriver webDriver;
-
-    @BeforeClass
-    public void setUpClass() {
-        String WebDriverPath = System.getenv("WebDrivers");
-        String os = System.getProperty("os.name");
-        if (os.startsWith("Windows")) {
-            WebDriverPath += "\\chromedriver.exe";
-        } else {
-            WebDriverPath += "/chromedriver";
-        }
-
-        System.setProperty("webdriver.chrome.driver", WebDriverPath);
-
-        webDriver = new ChromeDriver();
-        webDriver.get("https://ita-social-projects.github.io/GreenCityClient/#/");
-        LogInPO logInPO = new LogInPO(webDriver)
-                .clickSignInMenuButton()
-                .setEmail(email)
-                .setPassword(password)
-                .clickSignInButton();
-    }
+public class OpenPreviewPageTest extends BasicTest{
 
     @Test
     public void verifyButtonTextInPreviewPage() {
